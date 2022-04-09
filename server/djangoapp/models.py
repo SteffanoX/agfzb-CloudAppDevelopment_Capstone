ea85b671da5d45f4526cb52636a48f52eab1f137
SteffanoX <steffanosoh@gmail.com>
2022-04-09 10:04:40 +0000

    completed week 3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,21 +4,41 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+class CarMake(models.Model):
 # - Name
+    name = models.CharField(null=False, max_length=30)
 # - Description
+    description = models.CharField(null=True, max_length=500)
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+    def __str__(self):
+        return "name: " + self.name + ", " + \
+               "description: " + self.description
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+class CarModel(models.Model):
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
 # - Name
+    name = models.CharField(null=False, max_length=30)
 # - Dealer id, used to refer a dealer created in cloudant database
+    dealer_id = models.IntegerField()
 # - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+    SEDAN, SUV, WAGON, COUPE = 'Sedan', 'SUV', 'Wagon', 'Coupe'
+    car_type_options = [
+        (SEDAN, 'Sedan'), (SUV, 'SUV'), 
+        (WAGON, 'Wagon'), (COUPE, 'Coupe')
+        ]
+    car_type = models.CharField(null=False, max_length=20, choices=car_type_options, default=SEDAN)
 # - Year (DateField)
+    year = models.DateField(null=True)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+    def __str__(self):
+        return "Model Name: " + self.name + "," + \
+               "Dealer ID: " + str(self.dealer_id) + "," + \
+               "Car Type: " + self.car_type + "," + \
+               "year: " + str(self.year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
